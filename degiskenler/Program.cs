05352e834b743ea9ee2c98bf040e677262c88741
Samet ARTUL <smtartul@gmail.com>
2023-01-07 14:10:09 +0300

    variables examples added

--- degiskenler/Program.cs
+++ degiskenler/Program.cs
@@ -3,6 +3,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Degiskenler tanimlanacak");
+
+        byte b=1;
+        sbyte sb=2;
+
+        long l=10;
+
+        Int16 i16=22;
+        int i=111;
+        Int32 i32=1010;
+
+        float f=5.0f;
+        double d=0.2;
+        decimal de=1.89m;
+
+        char c='2';
+        string s="deneme";
+
+        bool b1=true;
+
+        DateTime dt=DateTime.Now;
     }
 }

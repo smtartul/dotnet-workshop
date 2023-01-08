629aa5a97d093660dbb01598a5577e7f841aaf46
Samet ARTUL <smtartul@gmail.com>
2023-01-08 13:45:22 +0300

    try-cath tasks

---
+++ try-catch/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace try_catch;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmis oldugunuz sayi:"+sayi); 
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine("Hata:"+ex.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("Islem tamamlandi.");
+        }
+      
+    }
+}

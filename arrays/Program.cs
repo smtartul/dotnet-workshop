3632d3d84b6731277ac2a73fdb0b177407d56103
Samet ARTUL <smtartul@gmail.com>
2023-01-08 16:40:52 +0300

    arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace arrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen ortalamasini hesaplamak istediginiz sayi adetini giriniz:");
+        int diziUzunlugu=int.Parse(Console.ReadLine());
+        int[] sayiDizisi=new int[diziUzunlugu];
+
+        for (int i = 0; i < diziUzunlugu; i++)
+        {
+            Console.Write("Lutfen {0}. sayiyi giriniz:",i+1);
+            sayiDizisi[i]=int.Parse(Console.ReadLine());
+        }
+
+        int toplam=0;
+        foreach (var sayi in sayiDizisi)
+        {
+            toplam+=sayi;
+        }
+
+        Console.WriteLine("Ortalama sonucu:"+toplam/diziUzunlugu);
+        Console.WriteLine("Girmis oldugunuz sayi adeti: "+sayiDizisi.Length);
+    }
+}

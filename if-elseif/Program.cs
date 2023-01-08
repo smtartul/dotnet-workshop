122880b327bccf1a80724f7eedab75233e6259be
Samet ARTUL <smtartul@gmail.com>
2023-01-08 14:12:58 +0300

    if-elseif

---
+++ if-elseif/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace if_elseif;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time =DateTime.Now.Hour;
+
+        if (time>=6 && time<11)
+        {
+            Console.WriteLine("Gunaydin");
+        }else if(time<=18){
+            Console.WriteLine("Iyi Gunler!");
+        }else {
+            Console.WriteLine("Iyi Geceler");
+        }
+
+        string res=time>=6 && time<11 
+            ? "Gunaydin" 
+            : time<=18 
+                ? "Iyi Gunler"
+                :"Iyi Geceler";
+        Console.WriteLine(res);
+    }
+}

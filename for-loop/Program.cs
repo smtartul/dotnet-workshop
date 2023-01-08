14546d50f872061b7ad60fce50c67fba07f539ac
Samet ARTUL <smtartul@gmail.com>
2023-01-08 15:27:07 +0300

    for loop task

---
+++ for-loop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace for_loop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ekrandan girilen sayiya kadar olan cift sayilari ekrana yazdir
+        Console.WriteLine("lutfen bir sayi giriniz");
+        int sayac=int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= sayac; i++)
+        {
+            if(i%2==0)
+            Console.WriteLine(i);
+        }
+    }
+}

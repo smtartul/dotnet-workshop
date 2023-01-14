87c33890133664ad2e2bbc7bd35d67162bc84921
Samet ARTUL <smtartul@gmail.com>
2023-01-14 17:54:52 +0300

    dotnet task4

---
+++ 04-task4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _04_task4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen bir cumle yaziniz");
+        string cumle=Console.ReadLine();
+
+        string [] arr=cumle.Split(' ');
+        string words=string.Join("",arr);
+        int kelimeAdet=arr.Count();
+        int harfAdet=words.Length;
+        if(harfAdet==0){
+            kelimeAdet=0;
+        }
+        
+        Console.WriteLine("Cumledeki toplam harf sayisi: "+harfAdet);
+        Console.WriteLine("Cumledeki toplam kelime sayisi: "+kelimeAdet);
+        
+
+    }
+}
---
+++ 04-task4/readme.md
@@ -0,0 +1,2 @@
+# Algortihm Task04
+## Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.

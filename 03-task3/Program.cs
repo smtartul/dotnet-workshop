bafecf7b7ca100cec169c7a073b654311d95aa98
Samet ARTUL <smtartul@gmail.com>
2023-01-14 17:31:32 +0300

    dotnet task3

---
+++ 03-task3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03_task3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lutfen pozitif bir sayi giriniz:");
+        List<string> dizi=new List<string>();
+        string sayi = Console.ReadLine();
+        bool res = int.TryParse(sayi, out int outSayi);
+        if (res && outSayi > 0)
+        {
+            Console.WriteLine("Lutfen {0} adet kelime giriniz", outSayi);
+            for (int i = 0; i < outSayi; i++)
+            {
+                Console.WriteLine("Lutfen {0}. kelime giriniz", i + 1);
+                string kelime=Console.ReadLine();
+                dizi.Add(kelime);
+            }
+            Console.WriteLine("Sonuc:");
+            dizi.Reverse();
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Hatali Giris Yaptiniz");
+        }
+    }
+}
---
+++ 03-task3/readme.md
@@ -0,0 +1,2 @@
+# Algorithm Task03
+## Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.

90d488fb504347e69d5eba5035d3c0f587d82dfa
Samet ARTUL <smtartul@gmail.com>
2023-01-14 15:14:13 +0300

    method overloading task

---
+++ method-overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace method_overloading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sayi = "999";
+
+        bool result = int.TryParse(sayi, out int outSayi);
+        if (result)
+        {
+            Console.WriteLine("Basarili!");
+            Console.WriteLine(outSayi);
+        }
+        else
+        {
+            Console.WriteLine("Basarisiz!");
+        }
+
+        Methods instance = new();
+        instance.Topla(4, 5, out int toplamSonucu);
+        Console.WriteLine(toplamSonucu);
+
+        int ifade = 999;
+        instance.EkranaYazdir(ifade);
+    }
+}
+class Methods
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a + b;
+
+    }
+
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+}

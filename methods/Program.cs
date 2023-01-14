b20c5c5b3be461df0f7d9150546af80a4102b16c
Samet ARTUL <smtartul@gmail.com>
2023-01-14 14:57:03 +0300

    methods task

---
+++ methods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace methods;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=2,b=3;
+        methodlar ornek =new();
+        ornek.EkranaYazdir(Convert.ToString(topla(a,b)));
+
+        int sonuc=ornek.ArttirVeTopla(a,b);
+        ornek.EkranaYazdir(Convert.ToString(sonuc));
+    }
+
+    static int topla(int sayi1,int sayi2){
+        return sayi1+sayi2;
+    }
+}
+class methodlar
+{
+    public void EkranaYazdir(string param){
+        Console.WriteLine(param);
+        }
+    public int ArttirVeTopla(int deger1,int deger2){
+        deger1++;
+        deger2++;
+        return (deger1+deger2);
+    }
+}

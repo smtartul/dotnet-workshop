07c3dc6acba26bd5f38fd2828b46662a95203e6f
Samet ARTUL <smtartul@gmail.com>
2023-01-15 18:50:06 +0300

    array-list

---
+++ array-list/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace array_list;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList arr = new ArrayList();
+        arr.Add("Ali");
+        arr.Add(2);
+        arr.Add(true);
+        arr.Add('a');
+
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("------ AddRange ----");
+        string[] renkler ={
+            "Kirmizi",
+            "Mavi",
+            "Yesil"
+        };
+        arr.AddRange(renkler);
+
+        List<int> sayilar = new List<int>(){
+            1,10,25,4,7
+        };
+
+        arr.AddRange(sayilar);
+
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}

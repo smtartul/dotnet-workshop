1f761a9c4b258c22e9c36faa1d5b17f936da6c06
Samet ARTUL <smtartul@gmail.com>
2023-01-16 19:44:07 +0300

    dotnet classes task

---
+++ classes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace classes;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Calisan calisan1=new Calisan();
+       calisan1.Ad="Samet"; 
+       calisan1.Soyad="ARTUL"; 
+       calisan1.No=33; 
+       calisan1.Departman="CENG"; 
+       calisan1.CalisanBilgileri();
+    }
+}
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Calisan Adi:{0}", Ad);
+        Console.WriteLine("Calisan Soyadi:{0}", Soyad);
+        Console.WriteLine("Calisan No:{0}", No);
+        Console.WriteLine("Calisan Departmani:{0}", Departman);
+    }
+}

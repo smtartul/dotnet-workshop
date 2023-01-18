6b428b200a4712741c65ffc1ec29350a86b72968
Samet ARTUL <smtartul@gmail.com>
2023-01-18 10:51:30 +0300

    dotnet enum

---
+++ enum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace enumTutorial;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazartesi);
+        Console.WriteLine((int)Gunler.Pazar);
+    }
+}
+
+enum Gunler
+{
+    Pazartesi,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
